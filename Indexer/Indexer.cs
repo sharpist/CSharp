451966a2f6111672bbbe60219fb083afcadad17d
Alexander Usov <alexusov@photographer.net>
2017-07-30 02:24:24 +0300

    Algorithms

--- Indexer/Indexer.cs
+++ Indexer/Indexer.cs
@@ -1,7 +1,50 @@
-﻿. . .
+﻿...
     class Program
     {
-     . . .
+        ...
+            var example = new Example<string>(5);
+
+            example[0] = "Hello";
+            example[1] = "World!";
+            example[2] = "How";
+            example[3] = "Are";
+            example[4] = "You?";
+
+
+            Console.Write($"{example[0]} {example[1]} {example[2]} {example[3]} {example[4]}");
+            // Hello World! How Are You?
+    }
+
+    struct Example<T>
+    {
+        private T[] arr;
+        public Example(int param)
+        { arr = new T[param]; }
+
+        public T this[int i] // индексатор
+        {
+            get { return this.arr[i]; }
+            set { this.arr[i] = value; }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+...
+    class Program
+    {
+        ...
             IntBits bits = new IntBits(126); // (126) 0111 1110
 
 
