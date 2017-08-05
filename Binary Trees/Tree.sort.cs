c1805f6f93a83dd7a2271e99a89e1c8482c978dc
Alexander Usov <alexusov@photographer.net>
2017-08-05 13:41:00 +0300

    Algorithms

--- Binary Trees/Tree.sort.cs
+++ Binary Trees/Tree.sort.cs
@@ -3,7 +3,7 @@
     {
         ...
             // данные для сортировки
-            int[] arr = new int[12] {5, 10, 0, 8, -12, 11, 5, -8, 15, 8, 10, 14};
+            int[] arr = new int[12] { 5, 10, 0, 8, -12, 11, 5, -8, 15, 8, 10, 14 };
 
             // заполнение
             int f;
@@ -17,9 +17,9 @@
 
     class Tree<T> where T : IComparable<T>
     {
-        private T Node { get; set; }        // узел
-        private Tree<T> Left { get; set; }  // левое поддерево
-        private Tree<T> Right { get; set; } // правое поддерево
+        private T Node { get; set; }              // узел
+        private Tree<T> Left { get; set; }        // левое поддерево
+        private Tree<T> Right { get; set; }       // правое поддерево
 
         public Tree(T param)
         {
@@ -28,38 +28,33 @@
             this.Right = null;
         }
 
-        public void Insert(T param)         // построение двоичного дерева
+        public void Insert(T param)               // построение двоичного дерева
         {
             T CurrentNode = this.Node;
 
-            if(CurrentNode.CompareTo(param) > 0)
+            if (CurrentNode.CompareTo(param) > 0) // узел > параметра true '+1', иначе false '0', '-1'
             {
-                if(this.Left == null)
-                    this.Left = new Tree<T>(param);
-                else
-                    this.Left.Insert(param);
-            }
-            else
-            {
-                if(this.Right == null)
-                    this.Right = new Tree<T>(param);
-                else
-                    this.Right.Insert(param);
-            }
-        }
+                if (this.Left == null) this.Left = new Tree<T>(param);   //        ____5____
+                else this.Left.Insert(param);                            //       /         \
+            }                                                            //      0          10
+            else                                                         //     /         /    \
+            {                                                            //   -12        8      11
+                if (this.Right == null) this.Right = new Tree<T>(param); //     \       / \    /  \
+                else this.Right.Insert(param);                           //     -8     5   8  10  15
+            }                                                            //                       /
+        }                                                                //                     14
 
-        public string Result()              // обход узлов в необходимом порядке
-        {                                   // вывод упорядоченных значений дерева
+        public string Result()                    // обход узлов в необходимом порядке
+        {                                         // вывод упорядоченных значений дерева
             string result = "";
 
-            if(this.Left != null)
-                result = this.Left.Result();
+            if (this.Left != null) result = this.Left.Result();
 
             result += $" {this.Node.ToString()} ";
 
-            if(this.Right != null)
-                result += this.Right.Result();
+            if (this.Right != null) result += this.Right.Result();
 
             return result;
         }
     }
+
