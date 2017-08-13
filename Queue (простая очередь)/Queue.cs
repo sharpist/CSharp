078bb70c056563f81418c234dcea0f26219ecc0e
Alexander Usov <alexusov@photographer.net>
2017-08-13 20:00:16 +0300

    Algorithms

--- Queue (простая очередь)/Queue.cs
+++ Queue (простая очередь)/Queue.cs
@@ -2,27 +2,29 @@
     class Program
     {
         ...
-            Queue<string> myQueue = new Queue<string>(3);
-            // 1. principle: "who came first, leaves first"
+            Queue<string> myQueue = new Queue<string>();
+            // 1. the principle of "who came first, leaves first"
             // 2. with expandable on demand
-            // 3. the extracted item will be removed from the queue
-            // 4. also the item can be deleted on demand
+            // 3. all items are added sequentially, priority for empty positions
+            // 4. you can add items by index
+            // 5. the extracted item is removed from the queue
+            // 6. also, you can delete items on demand
             myQueue.Add("First");
+            myQueue.Add(3, "Third");            // added to the third position
             myQueue.Add("Second");
-            myQueue.Add("Third");
-            Console.WriteLine(myQueue.Get(3));    // "Third"
-            Console.WriteLine(myQueue.Get());     // "First"
-            Console.WriteLine(myQueue.Get());     // "Second"
+            Console.WriteLine(myQueue.Get(3));  // "Third"
+            Console.WriteLine(myQueue.Get());   // "First"
+            Console.WriteLine(myQueue.Get());   // "Second"
 
             myQueue.Add("Sixth");
             myQueue.Add("Fifth");
             myQueue.Add("Fourth");
-            myQueue.Del(3);                       // Fourth now removed
-            myQueue.Sort();                       // Sixth, Fifth -> Fifth, Sixth abc...
+            myQueue.Del(3);                     // Fourth now removed
+            myQueue.Sort();                     // Sixth, Fifth -> Fifth, Sixth abc...
 
-            Console.WriteLine(myQueue.HowMany()); // "2"
-            Console.WriteLine(myQueue.Get());     // "Fifth"
-            Console.WriteLine(myQueue.Get());     // "Sixth"
+            Console.WriteLine(myQueue.Count()); // "2"
+            Console.WriteLine(myQueue.Get());   // "Fifth"
+            Console.WriteLine(myQueue.Get());   // "Sixth"
     }
 
     class Queue<T> where T : IComparable<T>
@@ -31,7 +33,17 @@
         public void Add(T item)
         {
             if (this.element == this.arr.Length) arrayCheck(1);
-            this.arr[this.element++] = item;
+            for (int i = 0; i < this.arr.Length; i++)
+            { if(arr[i] == null) this.arr[i] = item; }
+            this.element++;
+        }
+        public void Add(byte index, T item)
+        {
+            if (index <= 0) throw new IndexOutOfRangeException("Incorrect index!");
+            while (index - 1 >= this.arr.Length) { arrayCheck(1); }
+            arrayCheck(2, index);
+            this.arr[index - 1] = item;
+            this.element++;
         }
 
         // извлечь элемент
@@ -47,7 +59,7 @@
         public T Get(byte index)
         {
             if (this.element == 0) throw new IndexOutOfRangeException("The queue is empty!");
-            if(index <= 0) throw new IndexOutOfRangeException("Incorrect index!");
+            if (index <= 0) throw new IndexOutOfRangeException("Incorrect index!");
             var item = this.arr[index - 1];
             arrayCheck(-1, index);
             this.element--;
@@ -58,7 +70,8 @@
         // удалить элемент
         public void Del(byte index)
         {
-            if (this.element < index) throw new IndexOutOfRangeException("The queue does not carry this item!");
+            if (this.arr.Length < index - 1 || arr[index - 1] == null)
+                throw new IndexOutOfRangeException("The queue does not carry this item!");
             if (0 < index)
             {
                 arrayCheck(-1, index);
@@ -68,17 +81,17 @@
 
         // сортировать элементы
         public void Sort()
-        { if (this.arr.Length > 1) arrayCheck(2); }
+        { if (this.arr.Length > 1) arrayCheck(3); }
 
         // показать список
-        public int HowMany() => this.arr.Length;
-        
+        public int Count() => this.arr.Length;
+
 
         #region вспомогательный метод
         private void arrayCheck(sbyte param, byte index = 0)
         {
             switch (param) {
-                // увеличить массив (добавление элемента)
+                // увеличить массив (добавление следующего элемента)
                 case 1 :
                 T[] buf = new T[this.arr.Length];
 
@@ -89,6 +102,16 @@
 
                 for (int i = 0; i < buf.Length; i++)
                 { this.arr[i] = buf[i]; }
+                break;
+
+                // (добавление элемента по индексу)
+                case 2 :
+                buf = new T[this.arr.Length];
+                int j = 0;
+                for (int i = 0; i < this.arr.Length; i++)
+                { if (i != index - 1) buf[i] = this.arr[j++]; }
+
+                this.arr = buf;
                 break;
 
                 // уменьшить массив (извлечение следующего элемента)
@@ -105,7 +128,7 @@
                 // уменьшить массив (удаление/извлечение элемента по индексу)
                 case -1 :
                 buf = new T[this.arr.Length - 1];
-                int j = 0;
+                j = 0;
                 for (int i = 0; i < this.arr.Length; i++)
                 { if (i != index - 1) buf[j++] = this.arr[i]; }
 
@@ -113,9 +136,22 @@
                 break;
 
                 // сортировать элементы
-                case 2 :
-                for (int min = 0; min < this.arr.Length - 1; min++) {
-                    for (int max = min + 1; max < this.arr.Length; max++)
+                case 3 :
+                for (int i = 0; i < this.arr.Length; i++) {
+                    if (this.arr[i] == null) {
+                        for (j = i + 1; j < this.arr.Length; j++) {
+                            if (arr[j] != null) {
+                                var temp = this.arr[i];
+                                this.arr[i] = this.arr[j];
+                                this.arr[j] = temp;
+                                break;
+                            }
+                        }
+                    }
+                } byte k = 0;
+                for (int i = 0; i < this.arr.Length; i++) { if (arr[i] == null) k++; }
+                for (int min = 0; min < this.arr.Length - 1 - k; min++) {
+                    for (int max = min + 1; max < this.arr.Length - k; max++)
                     {
                         if (this.arr[min].CompareTo(this.arr[max]) >= 0)
                         {
@@ -138,6 +174,8 @@
         }
         public Queue(byte size) {
             this.element = 0;
+            if (0 == size) size++;
             this.arr = new T[this.element + size];
         }
     }
+
