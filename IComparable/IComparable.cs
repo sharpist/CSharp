9a6ef8eb59b8d16127edafb2c55132ff02fd5e69
Alexander Usov <alexusov@photographer.net>
2017-10-22 20:09:32 +0300

    Algorithms

---
+++ IComparable/IComparable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class TestType : IComparable<TestType>
+    {
+        public int Id { get; set; }
+
+
+        int IComparable<TestType>.CompareTo(TestType other)
+        {
+            if (other == null) // сравнивает объекты на основе поля Id
+                return 1;
+
+            if (this.Id > other.Id)
+                return 1;
+
+            if (this.Id < other.Id)
+                return -1;
+
+            return 0;
+        }
+    }
+}
