2ec67e98e705ff8180dc53a408e0ad1aa6ca4953
Alexander Usov <alexusov@photographer.net>
2017-11-07 18:27:05 +0300

    Algorithms

---
+++ Test Bitmap/Test Bitmap/Form.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Test_Bitmap
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.label = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.labelInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label.Location = new System.Drawing.Point(12, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(58, 20);
+            this.label.TabIndex = 0;
+            this.label.Text = "Image:";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(12, 32);
+            this.pictureBox.MaximumSize = new System.Drawing.Size(500, 500);
+            this.pictureBox.MinimumSize = new System.Drawing.Size(500, 500);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDraw.Location = new System.Drawing.Point(16, 538);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(75, 33);
+            this.buttonDraw.TabIndex = 2;
+            this.buttonDraw.Text = "Draw";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSave.Location = new System.Drawing.Point(128, 538);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 33);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelInfo.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.labelInfo.Location = new System.Drawing.Point(209, 544);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(0, 20);
+            this.labelInfo.TabIndex = 4;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 581);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.label);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(540, 620);
+            this.MinimumSize = new System.Drawing.Size(540, 620);
+            this.Name = "Form";
+            this.Text = "Test Bitmap";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label labelInfo;
+    }
+}
+
---
+++ Test Bitmap/Test Bitmap/Form.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+
+namespace Test_Bitmap
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private byte[] byteArray;
+
+        public Form()
+        {
+            InitializeComponent();
+
+            labelInfo.Text = "";
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            // загрузить изображение в byte array
+            using (var memoryStream = new System.IO.MemoryStream())
+            {
+                //Image image = Image.FromFile("image.png"); // из файла
+                Image image = Properties.Resources.image;  // из ресурса
+                image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+                byte[] byteArray = memoryStream.ToArray();
+
+                #region скопировать данные в поле byte array
+                this.byteArray = new byte[byteArray.Length];
+                for (int i = 0; i < byteArray.Length; ++i)
+                    this.byteArray[i] = byteArray[i];
+                #endregion
+            }
+            // byte array в bitmap
+            using (var memoryStream = new System.IO.MemoryStream(this.byteArray))
+            {
+                var bitmap = new System.Drawing.Bitmap(memoryStream);
+                pictureBox.Image = bitmap;
+            }
+
+            labelInfo.Text = "image uploaded!";
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (this.byteArray != null)
+            {
+                // преобразовать в bmp, записать на диск
+                using (var memoryStream = new System.IO.MemoryStream())
+                {
+                    foreach (byte b in this.byteArray) memoryStream.WriteByte(b);
+                    Image image = Image.FromStream(memoryStream);
+                    image.Save("newImage.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                }
+
+                labelInfo.Text = "image is converted to BMP and saved!";
+            }
+            else
+                labelInfo.Text = "need to download the image!";
+        }
+    }
+}
