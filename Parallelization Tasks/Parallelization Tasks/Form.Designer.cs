053edbf39c54daf9adb5ea3ff979f603850487e9
Alexander Usov <alexusov@photographer.net>
2017-11-08 22:39:11 +0300

    Algorithms

---
+++ Parallelization Tasks/Parallelization Tasks/Form.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Parallelization_Tasks
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.duration = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(797, 632);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 37);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // duration
+            // 
+            this.duration.AutoSize = true;
+            this.duration.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.duration.Location = new System.Drawing.Point(12, 632);
+            this.duration.Name = "duration";
+            this.duration.Size = new System.Drawing.Size(106, 20);
+            this.duration.TabIndex = 1;
+            this.duration.Text = "Время (мс):";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(25, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(700, 500);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 638);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 681);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.duration);
+            this.Controls.Add(this.buttonStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(900, 720);
+            this.MinimumSize = new System.Drawing.Size(900, 720);
+            this.Name = "Form";
+            this.Text = "Parallelization Tasks";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label duration;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Parallelization Tasks/Parallelization Tasks/Form.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+
+namespace Parallelization_Tasks
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        // горизонтальное и вертикальное разрешение для WriteableBitmap
+        private static int pixelWidth = 700;
+        private static int pixelHeight = 500;
+        // создать "пустой" (all-zeros) 24bpp Bitmap объект
+        Bitmap bmpRGB = new Bitmap(pixelWidth, pixelHeight, PixelFormat.Format24bppRgb);
+
+        // создать Rectangle и заблокировать растровое изображение в системной памяти
+        Rectangle rect;
+        BitmapData bmpData;
+
+        // рассчитать заполнение для 24bpp растрового изображения
+        int padding;
+
+
+        public Form()
+        {
+            InitializeComponent();
+
+            // вычисляется размер массива
+            int dataSize = bytesPerPixel * pixelWidth * pixelHeight;
+            data = new byte[dataSize];
+            // создает WriteableBitmap-объект
+            graphBitmap = new Bitmap(pixelWidth, pixelHeight);
+
+            rect = new Rectangle(0, 0, pixelWidth, pixelHeight);
+            bmpData = bmpRGB.LockBits(rect, ImageLockMode.WriteOnly, bmpRGB.PixelFormat);
+
+            padding = bmpData.Stride - 3 * pixelWidth;
+
+        }
+
+
+        private Bitmap graphBitmap = null; // используется для вывода графики в форме
+        private int bytesPerPixel = 3;              // класс WriteableBitmap использует на каждый пиксел 4 байта
+        private byte[] data;                        // байтовый массив для хранения данных графики
+
+        private byte redValue, greenValue, blueValue;
+
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // генерирует случайные значения для цветности RGB графики
+            Random rand = new Random();
+            redValue = (byte)rand.Next(0xFF);
+            greenValue = (byte)rand.Next(0xFF);
+            blueValue = (byte)rand.Next(0xFF);
+
+            // запуск таймера
+            Stopwatch watch = Stopwatch.StartNew(); // применяется для операций отсчета времени
+
+            // данные для всего изображения находятся между 0 и pixelWidth / 2
+            Task first = Task.Run(() => generateGraphData(data, 0, pixelWidth / 8)); // range is specified only for +X
+            Task second = Task.Run(() => generateGraphData(data, pixelWidth / 8, pixelWidth / 4));
+            Task third = Task.Run(() => generateGraphData(data, pixelWidth / 4, pixelWidth * 3 / 8));
+            Task fourth = Task.Run(() => generateGraphData(data, pixelWidth * 3 / 8, pixelWidth / 2));
+            Task.WaitAll(first, second, third, fourth); // task synchronization
+
+            // отображение времени, затраченного на создание данных
+            duration.Text = $"Время (мс): {watch.ElapsedMilliseconds}";
+
+
+
+            // разблокировка растрового изображения из системной памяти
+            bmpRGB.UnlockBits(bmpData);
+
+            // сохранить растровое изображение как BMP-файл в папке выполнения
+            //bmpRGB.Save("rgb_test.bmp", ImageFormat.Bmp);
+
+            pictureBox.Image = bmpRGB;
+
+        }
+
+        // заполняет массив данными для графики
+        private void generateGraphData(byte[] data, int partitionStart, int partitionEnd)
+        {
+            int Y = pixelHeight / 2, X = pixelWidth / 2;
+
+            for (short x = (short)partitionStart; x < partitionEnd; x++) // iterations = X
+            {
+                double p = Math.Sqrt(X * X - x * x);
+
+                for (var i = p; i > -p; i -= 4) // value X for -Y and Y
+                {
+                    double r = Math.Sqrt(x * x + i * i) / X;
+                    double q = (r - 1) * Math.Sin(5 * r);
+                    double y = i / 3 + (q * Y);
+
+                    #region plotXY
+                    // изображение зеркально относительно оси X поэтому метод plotXY вызывается дважды
+                    plotXY(data, (int)(-x + (pixelWidth / 2)), (int)(y + (pixelHeight / 2)));
+                    plotXY(data, (int)(x + (pixelWidth / 2)), (int)(y + (pixelHeight / 2)));
+                    #endregion
+                }
+            }
+        }
+
+        // устанавливает в массиве - данные байтов для каждой точки,
+        // значения байтов соответствуют X и Y координатам
+        private void plotXY(byte[] data, int x, int y)
+        {
+            unsafe
+            {
+                // присвоить указателю адрес первых пиксельных данных
+                byte* ptr = (byte*)bmpData.Scan0;
+                int index = (y * pixelWidth + x ) * bytesPerPixel;
+
+                        // назначить компонент RGB-значений к указателю
+                        ptr[index + 2] = redValue;
+                        ptr[index + 1] = greenValue;
+                        ptr[index] = blueValue;
+            }
+        }
+    }
+}
+
