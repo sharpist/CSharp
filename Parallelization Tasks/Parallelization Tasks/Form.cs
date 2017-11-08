27cf3e8198ba16bc67a9299b01ea4875d7ae1fdd
Alexander Usov <alexusov@photographer.net>
2017-11-08 23:53:46 +0300

    fixed external exception

--- Parallelization Tasks/Parallelization Tasks/Form.cs
+++ Parallelization Tasks/Parallelization Tasks/Form.cs
@@ -8,53 +8,44 @@ namespace Parallelization_Tasks
 {
     public partial class Form : System.Windows.Forms.Form
     {
-        // горизонтальное и вертикальное разрешение для WriteableBitmap
-        private static int pixelWidth = 700;
-        private static int pixelHeight = 500;
-        // создать "пустой" (all-zeros) 24bpp Bitmap объект
-        Bitmap bmpRGB = new Bitmap(pixelWidth, pixelHeight, PixelFormat.Format24bppRgb);
-
-        // создать Rectangle и заблокировать растровое изображение в системной памяти
-        Rectangle rect;
-        BitmapData bmpData;
-
-        // рассчитать заполнение для 24bpp растрового изображения
-        int padding;
-
-
-        public Form()
-        {
+        public Form() { // constructor
             InitializeComponent();
 
             // вычисляется размер массива
             int dataSize = bytesPerPixel * pixelWidth * pixelHeight;
             data = new byte[dataSize];
-            // создает WriteableBitmap-объект
-            graphBitmap = new Bitmap(pixelWidth, pixelHeight);
-
-            rect = new Rectangle(0, 0, pixelWidth, pixelHeight);
-            bmpData = bmpRGB.LockBits(rect, ImageLockMode.WriteOnly, bmpRGB.PixelFormat);
-
-            padding = bmpData.Stride - 3 * pixelWidth;
-
         }
 
+        // горизонтальное и вертикальное разрешение для Bitmap
+        private int pixelWidth = 700, pixelHeight = 500;
 
-        private Bitmap graphBitmap = null; // используется для вывода графики в форме
-        private int bytesPerPixel = 3;              // класс WriteableBitmap использует на каждый пиксел 4 байта
-        private byte[] data;                        // байтовый массив для хранения данных графики
+        private Bitmap bmpRGB;
+        private Rectangle rect;
+        private BitmapData bmpData;
 
-        private byte redValue, greenValue, blueValue;
+        private int bytesPerPixel = 3; // 1 пиксельное значение в 3 байтах
+        private byte[] data;           // байтовый массив для хранения данных графики
 
+        private byte redValue,
+                     greenValue,
+                     blueValue;
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
+            /// создать "пустой" (all-zeros) 24bpp Bitmap объект для вывода графики
+            bmpRGB = new Bitmap(pixelWidth, pixelHeight, PixelFormat.Format24bppRgb);
+            /// создать Rectangle и заблокировать растровое изображение в системной памяти
+            rect = new Rectangle(0, 0, pixelWidth, pixelHeight);
+            bmpData = bmpRGB.LockBits(rect, ImageLockMode.WriteOnly, bmpRGB.PixelFormat);
+
+
             // генерирует случайные значения для цветности RGB графики
             Random rand = new Random();
             redValue = (byte)rand.Next(0xFF);
             greenValue = (byte)rand.Next(0xFF);
             blueValue = (byte)rand.Next(0xFF);
 
+            #region RUNNING IN FLOWS
             // запуск таймера
             Stopwatch watch = Stopwatch.StartNew(); // применяется для операций отсчета времени
 
@@ -67,17 +58,14 @@ namespace Parallelization_Tasks
 
             // отображение времени, затраченного на создание данных
             duration.Text = $"Время (мс): {watch.ElapsedMilliseconds}";
+            #endregion
 
-
-
+            #region displaying
             // разблокировка растрового изображения из системной памяти
             bmpRGB.UnlockBits(bmpData);
-
-            // сохранить растровое изображение как BMP-файл в папке выполнения
-            //bmpRGB.Save("rgb_test.bmp", ImageFormat.Bmp);
-
+            // вывести графику в pictureBox
             pictureBox.Image = bmpRGB;
-
+            #endregion
         }
 
         // заполняет массив данными для графики
