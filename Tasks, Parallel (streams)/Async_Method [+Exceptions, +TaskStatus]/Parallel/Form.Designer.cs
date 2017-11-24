21823ed8d09d7ff6c488bb1997d6854b20c575c2
Alexander Usov <alexusov@photographer.net>
2017-11-24 04:42:00 +0300

    Algorithms

---
+++ Tasks, Parallel (streams)/Async_Method [+Exceptions, +TaskStatus]/Parallel/Form.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Async_Method
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.info = new System.Windows.Forms.Label();
+            this.infoStat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(17, 91);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 30);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStop.Location = new System.Drawing.Point(128, 91);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 30);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // info
+            // 
+            this.info.AutoSize = true;
+            this.info.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.info.Location = new System.Drawing.Point(13, 13);
+            this.info.MaximumSize = new System.Drawing.Size(430, 20);
+            this.info.MinimumSize = new System.Drawing.Size(430, 20);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(430, 20);
+            this.info.TabIndex = 2;
+            // 
+            // infoStat
+            // 
+            this.infoStat.AutoSize = true;
+            this.infoStat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.infoStat.Location = new System.Drawing.Point(13, 45);
+            this.infoStat.MaximumSize = new System.Drawing.Size(430, 20);
+            this.infoStat.MinimumSize = new System.Drawing.Size(430, 20);
+            this.infoStat.Name = "infoStat";
+            this.infoStat.Size = new System.Drawing.Size(430, 20);
+            this.infoStat.TabIndex = 3;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 131);
+            this.Controls.Add(this.infoStat);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(475, 170);
+            this.MinimumSize = new System.Drawing.Size(475, 170);
+            this.Name = "Form";
+            this.Text = "Parallel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label info;
+        private System.Windows.Forms.Label infoStat;
+    }
+}
+
---
+++ Tasks, Parallel (streams)/Async_Method [+Exceptions, +TaskStatus]/Parallel/README.md
@@ -0,0 +1,13 @@
+## Async_Method [+Exceptions, +TaskStatus]
+
+
+## 
+## “естовое приложение Parallel:
+
+* работа асинхронного метода с оператором await
+* перехват исключени€ OperationCanceledException
+* запрос статуса задачи
+
+![screen capture 1](example1.png)
+
+![screen capture 2](example2.png)
