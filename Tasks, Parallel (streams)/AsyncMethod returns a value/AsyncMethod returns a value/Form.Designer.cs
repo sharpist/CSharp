3239fe8cb744a7e739fe1f89f95df5b7dd3027a6
Alexander Usov <alexusov@photographer.net>
2017-12-20 02:26:42 +0300

    Algorithms

---
+++ Tasks, Parallel (streams)/AsyncMethod returns a value/AsyncMethod returns a value/Form.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace AsyncMethod_returns_a_value
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(197, 226);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Старт";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(4, 120);
+            this.textBox.MaximumSize = new System.Drawing.Size(275, 100);
+            this.textBox.MinimumSize = new System.Drawing.Size(275, 100);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(275, 100);
+            this.textBox.TabIndex = 1;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.StartButton);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form";
+            this.ShowIcon = false;
+            this.Text = "AsyncMethod returns a value";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ Tasks, Parallel (streams)/AsyncMethod returns a value/AsyncMethod returns a value/Form.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncMethod_returns_a_value
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form() { // конструктор
+            InitializeComponent();
+        }
+
+
+        private async void StartButton_Click(object sender, EventArgs e)
+        {
+            // await в вызове асинхронного метода возвращающего значение
+            var result = await MethodAsync();
+
+            for (int i = 0; i < 200; i++) { // вывод результата
+                textBox.Text += result[i].ToString() + (i+1<200?" ":"");
+            }
+        }
+
+
+        // асинхронный метод создающий результат
+        // возвращает Task<TResult>
+        private async Task<int[]> MethodAsync()
+        {
+            // длительная операция
+            Task<int[]> task = Task.Run(() => longOperation());
+            await       task;
+            return      task.Result;
+        }
+
+
+        // длительная операция
+        private int[] longOperation()
+        {
+            var ran = new Random();
+            var arr = new int[25000]; // заполнить массив
+            for (int i = 0; i < arr.Length; i++) { arr[i] = ran.Next(1, 99); }
+
+            // упорядочить значения
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                bool f = false;
+
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int buf = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = buf;
+
+                        f = true;
+                    }
+                }
+                if (f == false) break;
+            }
+            return arr;
+        }
+    }
+}
