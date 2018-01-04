9d2a6bb72a9a8293e77d1d58244f2aeae2aa12e4
Alexander Usov <alexusov@photographer.net>
2018-01-04 03:33:32 +0300

    Algorithms

---
+++ Delegates/FuncDelegate.OrderBy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Slide
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // отношение ключа к делегату
+            var keys = new Dictionary<string, Func<Person, object>>()
+            {
+                { "Id",   x => x.Id },
+                { "Name", x => x.Name }
+            };
+
+            // данные
+            var dataList = new List<Person>()
+            {
+                new Person() { Id = "3", Name = "Alexander"},
+                new Person() { Id = "2", Name = "Katerina"},
+                new Person() { Id = "4", Name = "Andrey"},
+                new Person() { Id = "1", Name = "Natalia"}
+            };
+
+
+            // выбранный ключ "Id" - опорное значение для упорядочивания
+            string property = "Id";
+
+
+            Func<Person, object> func;               // объявление делегата
+            keys.TryGetValue(property, out func);    // инициализация делегата по ключу
+
+            var sortedResult = Sort(dataList, func); // теперь выбран верный делегат
+
+
+            sortedResult?.ForEach(x => Console.WriteLine($"Id = {x.Id}, Name = {x.Name}"));
+            /* упорядочено по "Id"
+
+               Id = 1, Name = Natalia
+               Id = 2, Name = Katerina
+               Id = 3, Name = Alexander
+               Id = 4, Name = Andrey
+            */
+        }
+
+
+        // функция упорядочивания
+        public static List<Person> Sort(List<Person> dataList, Func<Person, object> p)
+            => dataList.OrderBy(p).ToList();
+    }
+
+    struct Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Delegates/README.md
@@ -0,0 +1,14 @@
+## FuncDelegate.OrderBy
+
+
+* Sort data (employee info) by the selected criterion "Id", "Name" or other property.
+
+* ”пор€дочить имеющиес€ данные (инфо о сотрудниках) по выбранному критерию "Id", "Name" или другое свойство.
+
+
+## Employee info:
+![screen capture 1](data.png)
+
+## Sorting criterion (keys):
+![screen capture 1](keys.png)
+
