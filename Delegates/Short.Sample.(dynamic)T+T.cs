3791e6887e7fe5822d0399922d4ff317ad0708c2
Alexander Usov <alexusov@photographer.net>
2018-01-16 17:29:38 +0300

    Algorithms

--- Delegates/Short.Sample.(dynamic)T+T.cs
+++ Delegates/Short.Sample.(dynamic)T+T.cs
@@ -6,14 +6,14 @@ namespace Example
     {
         // делегат Func ссылается на метод MetodSumming,
         // в который передаются аргументы arg1 и arg2
-        static T Calc<T>(Func<T, T, T> f, T arg1, T arg2)
+        static TResult Calc<T1, T2, TResult>(Func<T1, T2, TResult> f, T1 arg1, T2 arg2)
             => f(arg1, arg2);
 
 
 
         // метод MetodSumming на который указывает делегат
         // возвращает сумму значений
-        static T MetodSumming<T>(T param1, T param2)
+        static TResult MetodSumming<T1, T2, TResult>(T1 param1, T2 param2)
             => (dynamic)param1 + param2;
 
 
@@ -22,19 +22,18 @@ namespace Example
         // тестируем class Program
         static void Main()
         {
-            Console.WriteLine(Calc(MetodSumming, 12, 13));               // 25
+            Console.WriteLine(Calc(MetodSumming<int, int, int>,                  12,  13)); // 25
 
-            Console.WriteLine(Calc(MetodSumming, 2.9, 1.1));             // 4
+            Console.WriteLine(Calc(MetodSumming<double, double, double>,         2.9, 1.1)); // 4
 
-            Console.WriteLine(Calc(MetodSumming, "C", "#"));             // C#
+            Console.WriteLine(Calc(MetodSumming<string, char, string>,           "C", '#')); // C#
 
             // используется собственный тип объекта
-            Console.WriteLine(Calc(MetodSumming, new ObjectType(6),
-                                                 new ObjectType(4)));    // 10
+            Console.WriteLine(Calc(MetodSumming<ObjectType, ObjectType, object>, new ObjectType(6), new ObjectType(4))); // 10
 
-            Console.WriteLine(Calc(MetodSumming, new ObjectType(6), 4)); // 10
+            Console.WriteLine(Calc(MetodSumming<ObjectType, int, object>,        new ObjectType(6), 4)); // 10
 
-            Console.WriteLine(Calc(MetodSumming, 6, new ObjectType(4))); // 10
+            Console.WriteLine(Calc(MetodSumming<int, ObjectType, object>,        6, new ObjectType(4))); // 10
         }
     }
 
