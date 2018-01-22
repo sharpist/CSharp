89c201348caf2692cb8a7f2e5c7ea07ee6a9e574
Alexander Usov <alexusov@photographer.net>
2018-01-23 02:51:45 +0300

    Algorithms

---
+++ Tasks, Parallel (streams)/Parallel.Invoke.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        ConcurrentQueue<int> cq = new ConcurrentQueue<int>();
+
+        for (int i = 0; i < 1000; i++) cq.Enqueue(i);
+
+
+        int result = 0;
+        Action action = () =>
+        {
+            int value, sum = 0;
+            while (cq.TryDequeue(out value)) sum += value; // извлечение и суммирование значений
+
+            Interlocked.Add(ref result, sum); // потокобезопасная операция присвоения результата
+        };
+        // начать 4 одновременных действия
+        Parallel.Invoke(action, action, action, action);
+
+        Console.WriteLine($"result = {result}"); // 499500
+    }
+}
