f8bc12904e1346c30aef92ba95379b5c77100ae5
Alexander Usov <alexusov@photographer.net>
2018-02-28 15:21:19 +0300

    Algorithms

---
+++ Graph/DrawPoints/DrawPoints.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox.BackColor = Color.Black;
+        }
+        int i;
+        private Graphics g;
+        private void pictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            g = (sender as Control).CreateGraphics(); // создать холст из контрола
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality; // добовить размытие
+            g.FillEllipse(new SolidBrush(Color.White), e.X, e.Y, 5, 5); // нарисовать точку
+
+            if (i++ == 3) { i = 0; pictureBox.Refresh(); } // очистить холст
+            g.Dispose(); // освободить ресурсы
+        }
+    }
+}
---
+++ Graph/DrawPoints/README.md
@@ -0,0 +1,5 @@
+## Draw Points
+
+## Demonstration
+![screen capture 1](01.gif)
+
