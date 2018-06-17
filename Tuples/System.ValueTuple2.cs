34cedde7aabf69b368ce3aa2272deba73f08dc29
Alexander Usov <alexusov@photographer.net>
2018-06-17 04:39:53 +0300

    Algorithms

---
+++ Tuples/System.ValueTuple2.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        var result = Method();
+
+        WriteLine($"{result.Item1} {result.Item2} {result.Item3}");
+        // Hello World !
+    }
+
+    static (string, string, char) Method()
+    {
+        // Note:
+        // tuple syntax is involved to returns
+        // a set of values of different types
+        return ("Hello", "World", '!');
+    }
+}
