611f4d1984432b9e64e97ffc15d2a82a787fa523
Alexander Usov <alexusov@photographer.net>
2018-06-20 12:53:53 +0300

    Algorithms

--- Loops/Loop Unwinding (optimization).cs
+++ Loops/Loop Unwinding (optimization).cs
@@ -3,67 +3,64 @@ using static System.Console;
 
 class Program
 {
-    static Stopwatch watch;
-    static int[] A = new int[70000000],
-                 B = new int[70000000];
-
     static void Main()
     {
+        uint size = 80000000;
+        var A = new int[size];
         #region "B" array filling
-        for (int i = 0; i < 70000000; i++)
-                 B[i] = i + 1;
+        var B = new int[size];
+        for (int i = 0; i < size; i++)
+            B[i] = i + 1;
         #endregion
 
 
-        watch = Stopwatch.StartNew();
-        NormalLoop();
+        var watch = Stopwatch.StartNew();
+        NormalLoop(size, A, B);
         watch.Stop();
         // Normal loop
-        WriteLine("Elapsed ms: " + watch.ElapsedMilliseconds); // Elapsed ms: ~1680
+        WriteLine("Elapsed ms: " + watch.ElapsedMilliseconds); // Elapsed ms: ~614
 
         watch = Stopwatch.StartNew();
-        AfterLoopUnrolling();
+        AfterLoopUnrolling(size, A, B);
         watch.Stop();
         // After loop unrolling
-        WriteLine("Elapsed ms: " + watch.ElapsedMilliseconds); // Elapsed ms: ~1400
+        WriteLine("Elapsed ms: " + watch.ElapsedMilliseconds); // Elapsed ms: ~320
 
         watch = Stopwatch.StartNew();
-        DuffsDevice();
+        DuffsDevice(size, A, B);
         watch.Stop();
         // Duff's device a functionally equivalent version
-        WriteLine("Elapsed ms: " + watch.ElapsedMilliseconds); // Elapsed ms: ~380 (not used "Mod" operation)
+        WriteLine("Elapsed ms: " + watch.ElapsedMilliseconds); // Elapsed ms: ~307
     }
 
-    static void NormalLoop()
+    static void NormalLoop(uint count, int[] A, int[] B)
     {
-        for (int i = 0; i < 70000000; i++)
+        for (int i = 0; i < count; i++)
         {
-            A[i] = (i % B[i]);
+            A[i] = B[i];
         }
     }
 
     // ***this optimization consisting in an artificial
     // increase in the number of instructions
-    static void AfterLoopUnrolling()
+    static void AfterLoopUnrolling(uint count, int[] A, int[] B)
     {
-        for (int i = 0; i < 70000000 - 4; i += 5)
+        for (int i = 0; i < count - 4; i += 5)
         {
-            A[i] = (i % B[i]);
-            A[i + 1] = ((i + 1) % B[i + 1]);
-            A[i + 2] = ((i + 2) % B[i + 2]);
-            A[i + 3] = ((i + 3) % B[i + 3]);
-            A[i + 4] = ((i + 4) % B[i + 4]);
+            A[i] = B[i];
+            A[i + 1] = B[i + 1];
+            A[i + 2] = B[i + 2];
+            A[i + 3] = B[i + 3];
+            A[i + 4] = B[i + 4];
         }
     }
 
     // ***Duff's device is a way of manually implementing loop unrolling by
     // interleaving two syntactic constructs the while loop and a switch statement
-    static void DuffsDevice()
+    static void DuffsDevice(uint count, int[] A, int[] B)
     {
-        int i = 0;
-        int count = 70000000;
-
-        int n = (count + 7) / 8;
+        uint i = 0;
+        uint n = (count + 7) / 8;
         switch (count % 8) {
             case 0: A[i] = B[i++]; goto case 7;
             case 7: A[i] = B[i++]; goto case 6;
@@ -75,14 +72,15 @@ class Program
             case 1: A[i] = B[i++]; break;
         }
         while (--n > 0) {
-            A[i] = B[i++];
-            A[i] = B[i++];
-            A[i] = B[i++];
-            A[i] = B[i++];
-            A[i] = B[i++];
-            A[i] = B[i++];
-            A[i] = B[i++];
-            A[i] = B[i++];
+            A[i] = B[i];
+            A[i + 1] = B[i + 1];
+            A[i + 2] = B[i + 2];
+            A[i + 3] = B[i + 3];
+            A[i + 4] = B[i + 4];
+            A[i + 5] = B[i + 5];
+            A[i + 6] = B[i + 6];
+            A[i + 7] = B[i + 7];
+            i += 8;
         }
     }
 }
