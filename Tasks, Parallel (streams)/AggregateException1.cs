545ffa5bf5a9450fb5b0887c85a24791bbd67b2c
Alexander Usov <alexusov@photographer.net>
2018-07-10 22:30:49 +0300

    C_Sharp

---
+++ Tasks, Parallel (streams)/AggregateException1.cs
@@ -0,0 +1,93 @@
+﻿namespace PLINQCancellation_1
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using static System.Console;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] source = Enumerable.Range(1, 10000000).ToArray();
+            var cts = new CancellationTokenSource();
+
+            // Start a new asynchronous task that will cancel the 
+            // operation from another thread. Typically you would call
+            // Cancel() in response to a button click or some other
+            // user interface event.
+            Task.Factory.StartNew(() =>
+            {
+                UserClicksTheCancelButton(cts);
+            });
+
+            int[] results = null;
+            try
+            {
+                results = (from num in source.AsParallel().WithCancellation(cts.Token)
+                           where num % 3 == 0
+                           orderby num descending
+                           select num).ToArray();
+            }
+            catch (OperationCanceledException e)
+            {
+                WriteLine(e.Message);
+            }
+            catch (AggregateException ae)
+            {
+                if (ae.InnerExceptions != null)
+                {
+                    foreach (Exception e in ae.InnerExceptions)
+                        WriteLine(e.Message);
+                }
+            }
+            finally
+            {
+                cts.Dispose();
+            }
+
+            if (results != null)
+            {
+                foreach (var v in results)
+                    WriteLine(v);
+            }
+            WriteLine();
+            ReadKey();
+        }
+
+        static void UserClicksTheCancelButton(CancellationTokenSource cts)
+        {
+            // Wait between 150 and 500 ms, then cancel.
+            // Adjust these values if necessary to make
+            // cancellation fire while query is still executing.
+            Random rand = new Random();
+            Thread.Sleep(rand.Next(150, 500));
+            cts.Cancel();
+        }
+    }
+}
+
+/*
+Платформа PLINQ не помещает единственное OperationCanceledException в
+System.AggregateException. OperationCanceledException нужно обрабатывать
+в отдельном блоке catch.
+
+Если один или несколько пользовательских делегатов создают исключение
+OperationCanceledException(externalCT) (с помощью внешнего объекта
+System.Threading.CancellationToken), но не создают других исключений, и
+при этом запрос был определен как AsParallel().WithCancellation(externalCT),
+то PLINQ выдает одно исключение OperationCanceledException(externalCT), но
+не System.AggregateException.
+
+Тем не менее, если один пользовательский делегат создает исключение
+OperationCanceledException, а другой делегат создает исключение другого
+типа, то оба этих исключения помещаются в AggregateException.
+
+Общие рекомендации по отмене: 
+Если отменяется пользовательский делегат, необходимо известить PLINQ о внешнем
+CancellationToken и создать исключение OperationCanceledException(externalCT).
+
+Если выполняется только отмена и нет других исключений, следует обрабатывать
+OperationCanceledException, а не AggregateException. 
+*/
---
+++ Tasks, Parallel (streams)/AggregateException2.cs
@@ -0,0 +1,105 @@
+﻿namespace PLINQCancellation_2
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using static System.Console;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] source = Enumerable.Range(1, 10000000).ToArray();
+            var cts = new CancellationTokenSource();
+
+            // Start a new asynchronous task that will cancel the 
+            // operation from another thread. Typically you would call
+            // Cancel() in response to a button click or some other
+            // user interface event.
+            Task.Factory.StartNew(() =>
+            {
+                UserClicksTheCancelButton(cts);
+            });
+
+            double[] results = null;
+            try
+            {
+                results = (from num in source.AsParallel().WithCancellation(cts.Token)
+                           where num % 3 == 0
+                           select Function(num, cts.Token)).ToArray();
+            }
+            catch (OperationCanceledException e)
+            {
+                WriteLine(e.Message);
+            }
+            catch (AggregateException ae)
+            {
+                if (ae.InnerExceptions != null)
+                {
+                    foreach (Exception e in ae.InnerExceptions)
+                        WriteLine(e.Message);
+                }
+            }
+            finally
+            {
+                cts.Dispose();
+            }
+
+            if (results != null)
+            {
+                foreach (var v in results)
+                    WriteLine(v);
+            }
+            WriteLine();
+            ReadKey();
+        }
+
+        // A toy method to simulate work.
+        static double Function(int n, CancellationToken ct)
+        {
+            // If work is expected to take longer than 1 ms
+            // then try to check cancellation status more
+            // often within that work.
+            for (int i = 0; i < 5; i++)
+            {
+                // Work hard for approx 1 millisecond.
+                Thread.SpinWait(50000);
+
+                // Check for cancellation request.
+                ct.ThrowIfCancellationRequested();
+            }
+            // Anything will do for our purposes.
+            return Math.Sqrt(n);
+        }
+
+        static void UserClicksTheCancelButton(CancellationTokenSource cts)
+        {
+            // Wait between 150 and 500 ms, then cancel.
+            // Adjust these values if necessary to make
+            // cancellation fire while query is still executing.
+            Random rand = new Random();
+            Thread.Sleep(rand.Next(150, 500));
+            WriteLine("Press 'c' to cancel");
+            if (ReadKey().KeyChar == 'c')
+                cts.Cancel();
+        }
+    }
+}
+
+/*
+При обработке отмены в пользовательском коде, нет необходимости использовать
+WithCancellation в определении запроса.
+Но рекомендуется это сделать, так как WithCancellation не влияет на производительность
+запросов, но позволяет обрабатывать отмену как в операторах запроса, так и в
+пользовательском коде.
+
+Чтобы обеспечить высокую скорость реагирования системы, рекомендуется проверять
+отмену приблизительно каждую миллисекунду. Впрочем, здесь считается допустимым любой
+период вплоть до 10 мс.
+Частота проверок должна быть такой, чтобы не снижать производительность кода.
+
+Если удаляется перечислитель, например когда код прерывается вне цикла foreach,
+выполняющего итерацию по результатам запроса, то этот запрос отменяется без
+создания исключений. 
+*/
