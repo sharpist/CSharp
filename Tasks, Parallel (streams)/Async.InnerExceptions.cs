2784c0e6047a1fb6ffbc4b9ceb960ff6404bfa35
Alexander Usov <alexusov@photographer.net>
2018-07-10 18:37:58 +0300

    C_Sharp

---
+++ Tasks, Parallel (streams)/Async.InnerExceptions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using static System.Console;
+
+public class Example
+{
+    private static async Task DoMultipleAsync()
+    {
+        Task task1 = ExcAsync(info: "First Task");
+        Task task2 = ExcAsync(info: "Second Task");
+        Task task3 = ExcAsync(info: "Third Task");
+        Task tasks = Task.WhenAll(task1, task2, task3);
+
+        try
+        {
+            await tasks;
+        }
+        catch (Exception ex)
+        {
+            WriteLine($"Exception: {ex.Message}");
+            WriteLine($"Task IsFaulted: {tasks.IsFaulted}");
+
+            foreach (var inEx in tasks.Exception.InnerExceptions)
+                WriteLine($"Task Inner Exception: {inEx.Message}");
+        }
+
+        async Task ExcAsync(string info) // local async function
+        {
+            await Task.Delay(100);
+            throw new Exception($"Error-{info}");
+        }
+    }
+    // Exception: Error-First Task
+    // Task IsFaulted: True
+    // Task Inner Exception: Error-First Task
+    // Task Inner Exception: Error-Second Task
+    // Task Inner Exception: Error-Third Task
+
+    public static void Main() => DoMultipleAsync().Wait();
+}
