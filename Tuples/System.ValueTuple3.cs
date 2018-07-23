b19856b13dabc73ae49c4e45aa8fd1b061fbf259
Alexander Usov <alexusov@photographer.net>
2018-07-23 13:51:11 +0300

    C_Sharp

---
+++ Tuples/System.ValueTuple3.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+class Program
+{ // переменные x и y меняются значениями
+  // использован ValueTuple
+    static void Main()
+    {
+        Swap();
+    }
+
+    static void Swap()
+    {
+        int x = 0, y = 42;
+
+        (x, y) = (y, x);
+
+        WriteLine($"now x: {x}, y: {y}");
+        // now x: 42, y: 0
+    }
+}
