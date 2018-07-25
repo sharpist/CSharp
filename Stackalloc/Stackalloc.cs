17b7daada1ec327b14ed10569a8401d369eea422
Alexander Usov <alexusov@photographer.net>
2018-07-25 16:06:44 +0300

    C_Sharp

---
+++ Stackalloc/Stackalloc.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        unsafe
+        {
+            const int arraySize = 20;
+            int* fib = stackalloc int[arraySize];
+            int* p = fib;
+            // начало последовательности с 1, 1
+            *p++ = *p++ = 1;
+
+            for (int i = 2; i < arraySize; ++i, ++p)
+                // рассчитывается сумма предыдущих пар чисел
+                *p = p[-1] + p[-2];
+
+
+            for (int i = 0; i < arraySize; ++i) WriteLine(fib[i]);
+            // 1
+            // 1
+            // 2
+            // 3
+            // 5
+            // 8
+            // 13
+            // 21
+            // 34
+            // 55
+            // 89
+            // 144
+            // 233
+            // 377
+            // 610
+            // 987
+            // 1597
+            // 2584
+            // 4181
+            // 6765
+        }
+    }
+}
