f38bb41378dfa182f9e89c88319b63c10ac82d2b
Alexander Usov <alexusov@photographer.net>
2018-07-27 01:20:27 +0300

    C_Sharp

---
+++ Attributes/CreatingCustomAttributes.cs
@@ -0,0 +1,25 @@
+﻿// многократно используемый настраиваемый атрибут
+[System.AttributeUsage(System.AttributeTargets.Class |
+                       System.AttributeTargets.Struct,
+                       AllowMultiple = true)
+]
+public class Author : System.Attribute
+{
+    private string name;
+    public double version;
+
+    public Author(string name)
+    {
+        this.name = name;
+        version = 1.0;
+    }
+}
+
+// несколько атрибутов одного типа применяются к классу
+[Author("P. Ackerman", version = 1.1)]
+[Author("R. Koch", version = 1.2)]
+class SampleClass
+{
+    // P. Ackerman's code goes here...  
+    // R. Koch's code goes here...  
+}
