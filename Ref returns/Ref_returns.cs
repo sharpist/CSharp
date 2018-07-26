a1cfde810163a025a50b304c88a3e5fd18ef9bc4
Alexander Usov <alexusov@photographer.net>
2018-07-26 03:45:49 +0300

    C_Sharp

---
+++ Ref returns/Ref_returns.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        var store = new NumberStore();
+        WriteLine($"Исходная последовательность: {store.ToString()}");
+
+        ref var value = ref store.FindNumber(16);
+        value *= 2;
+        WriteLine($"Новая последовательность:    {store.ToString()}");
+        // Исходная последовательность: 1 3 7 15 31 63 127 255 511 1023
+        // Новая последовательность:    1 3 7 15 62 63 127 255 511 1023
+    }
+}
+class NumberStore
+{
+    int[] numbers = { 1, 3, 7, 15, 31, 63, 127, 255, 511, 1023 };
+
+    public ref int FindNumber(int target)
+    {
+        for (int ctr = 0; ctr < numbers.Length; ctr++)
+            if (numbers[ctr] >= target)
+                return ref numbers[ctr];
+
+        return ref numbers[0];
+    }
+
+    public override string ToString() => string.Join(" ", numbers);
+}
---
+++ Ref returns/Ref_returns2.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        var store = new NumberStore();
+        WriteLine($"Исходная последовательность: {store.ToString()}");
+
+        ref var value = ref store.FindNumber(16);
+        value *= 2;
+        WriteLine($"Новая последовательность:    {store.ToString()}");
+        // Исходная последовательность: 1 3 7 15 31 63 127 255 511 1023
+        // Новая последовательность:    1 3 7 15 62 63 127 255 511 1023
+    }
+}
+class NumberStore
+{
+    int[] numbers = { 1, 3, 7, 15, 31, 63, 127, 255, 511, 1023 };
+
+    public ref int FindNumber(int target)
+    {
+        ref int returnVal = ref numbers[0];
+        var ctr = numbers.Length - 1;
+        while ((ctr > 0) && numbers[ctr] >= target)
+        {
+            returnVal = ref numbers[ctr];
+            ctr--;
+        }
+        return ref returnVal;
+    }
+
+    public override string ToString() => string.Join(" ", numbers);
+}
