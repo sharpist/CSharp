864538719262268df5c899424a87d366fb444f77
Alexander Usov <alexusov@photographer.net>
2018-07-27 23:45:25 +0300

    C_Sharp

--- Attributes/AccessingAttributes.cs
+++ Attributes/AccessingAttributes.cs
@@ -2,7 +2,8 @@
 
 // многократно используемый настраиваемый атрибут
 [System.AttributeUsage(System.AttributeTargets.Class |
-                       System.AttributeTargets.Struct,
+                       System.AttributeTargets.Struct |
+                       System.AttributeTargets.Method,
                        AllowMultiple = true)
 ]
 public class Author : System.Attribute
@@ -20,11 +21,18 @@ public class Author : System.Attribute
 
 // несколько атрибутов одного типа применяются к классу
 [Author("P. Ackerman")]
-[Author("R. Koch", version = 2.0)]
+[Author("R. Koch", version = 1.3)]
 public class SampleClass
 {
-    // P. Ackerman's code goes here...  
-    // R. Koch's code goes here...  
+    // P. Ackerman's code goes here...
+    // R. Koch's code goes here...
+
+    // атрибут применяется к методу класса
+    [Author("A. Carpenter", version = 2.0)]
+    public void SampleMethod()
+    {
+        // A. Carpenter's code goes here...
+    }
 }
 
 class TestAuthorAttribute
@@ -33,7 +41,7 @@ class TestAuthorAttribute
 
     private static void PrintAuthorInfo(System.Type t)
     {
-        WriteLine($"Информация об авторах для {t}:");
+        WriteLine($"Информация об авторах класса {t}:");
         // использование отражения
         var attrs = System.Attribute.GetCustomAttributes(t);
 
@@ -43,10 +51,24 @@ class TestAuthorAttribute
                 var a = (Author)attr;
                 WriteLine($"\t{a.GetName()},\tверсия {a.version:f}");
             }
+
+
+        // найти атрибуты на уровне методов
+        WriteLine($"\nИнформация об авторах методов класса {t}:");
+        var meths = t.GetMethods();
+        foreach (var meth in meths) {
+            var a = (Author)System.Attribute.GetCustomAttribute(meth, typeof(Author));
+            if (a != null)
+                WriteLine($"\t{a.GetName()},\tверсия {a.version:f}");
+            WriteLine($"Нет атрибута в функции члена {meth.ToString()}");
+        }
     }
-    // Информация об авторах для SampleClass:
+    // Информация об авторах класса SampleClass:
     //         P. Ackerman,    версия 1,00
-    //         R. Koch,        версия 2,00
+    //         R. Koch,        версия 1,30
+    //
+    // Информация об авторах методов класса SampleClass:
+    //         A. Carpenter,   версия 2,00
 }
 
 class Program { static void Main() => TestAuthorAttribute.Test(); }
--- Attributes/CreatingCustomAttributes.cs
+++ Attributes/CreatingCustomAttributes.cs
@@ -1,11 +1,12 @@
 ﻿// многократно используемый настраиваемый атрибут
 [System.AttributeUsage(System.AttributeTargets.Class |
-                       System.AttributeTargets.Struct,
+                       System.AttributeTargets.Struct |
+                       System.AttributeTargets.Method,
                        AllowMultiple = true)
 ]
 public class Author : System.Attribute
 {
-    private string name;
+    string name;
     public double version;
 
     public Author(string name)
@@ -13,13 +14,21 @@ public class Author : System.Attribute
         this.name = name;
         version = 1.0;
     }
+    public string GetName() => this.name;
 }
 
 // несколько атрибутов одного типа применяются к классу
-[Author("P. Ackerman", version = 1.1)]
-[Author("R. Koch", version = 1.2)]
-class SampleClass
+[Author("P. Ackerman")]
+[Author("R. Koch", version = 1.3)]
+public class SampleClass
 {
-    // P. Ackerman's code goes here...  
-    // R. Koch's code goes here...  
+    // P. Ackerman's code goes here...
+    // R. Koch's code goes here...
+
+    // атрибут применяется к методу класса
+    [Author("A. Carpenter", version = 2.0)]
+    public void SampleMethod()
+    {
+        // A. Carpenter's code goes here...
+    }
 }
