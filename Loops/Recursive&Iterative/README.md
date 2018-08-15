9fcd06209402b3668908f7507c281f33b83af9e2
Alexander Usov <alexusov@photographer.net>
2018-08-15 23:41:45 +0300

    C_Sharp

---
+++ Loops/Recursive&Iterative/README.md
@@ -0,0 +1,17 @@
+## Recursive call vs. Iterative call
+
+"Recursive" call (without tail optimization) consumes the stack and is can be of causing stack overflow in contrast to "Iterative" call which doesn't consume the stack.
+___
+
+### Demonstration of effectiveness: ###
+
+![screen capture 1](01.gif)
+
+#### Recursion implementation ####
+
+![screen capture 2](recursion.png)
+
+#### Iterative implementation ####
+
+![screen capture 3](iteration.png)
+
---
+++ Loops/Recursive&Iterative/source.cs
@@ -0,0 +1,49 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        var prm = new Program();
+        prm.Iteration();
+        prm.Recursion();
+    }
+
+
+    public void Iteration()
+    {
+        for (int i = 0; i < 5000; i++)
+        {
+            // Fibonacci
+            var fib = new int[2000];
+            for (int j = 0; j != fib.Length - 2; j++)
+            {
+                fib[j + 2] = (j != 0) ?
+                    fib[j + 1] + fib[j] : (fib[j + 1] = fib[j] = 1) + fib[j];
+            }
+        }
+    }
+
+
+    public void Recursion(int i = 0)
+    {
+        if (i++ == 5000)          // exit
+            return;
+
+        fibonacci(new int[2000]); // any custom operations
+
+        Recursion(i);             // recursive call
+
+
+        // local Fibonacci procedure
+        void fibonacci(int[] fib, int j = 0)
+        {
+            fib[j + 2] = (j != 0) ?
+                fib[j + 1] + fib[j] : (fib[j + 1] = fib[j] = 1) + fib[j];
+
+            if (++j == fib.Length - 2) // exit
+                return;
+
+            fibonacci(fib, j);         // recursive call
+        }
+    }
+}
