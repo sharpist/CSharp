3c54dc483b49549e0ab8c9677fe146f1faa382ae
Alexander Usov <alexusov@photographer.net>
2018-08-25 05:22:07 +0300

    C_Sharp

--- Binary Trees + IEnumerable/Binary Search Tree.cs
+++ Binary Trees + IEnumerable/Binary Search Tree.cs
@@ -3,6 +3,95 @@ using System.Collections.Generic;
 
 namespace BST
 {
+    class BinarySearchTree<TKey, TValue> where TKey : IComparable<TKey>
+    {
+        private class BinarySearchTreeElement
+        {
+            public TKey   Key   { get; set; } = default;
+            public TValue Value { get; set; } = default;
+
+            public BinarySearchTree<TKey, TValue> Left  { get; set; }
+            public BinarySearchTree<TKey, TValue> Right { get; set; }
+        }
+        private BinarySearchTreeElement root;
+
+        public BinarySearchTree(TKey key, TValue value)
+        {
+            this.root = new BinarySearchTreeElement();
+
+            this.root.Key   = key;
+            this.root.Value = value;
+
+            this.root.Left  = null;
+            this.root.Right = null;
+        }
+
+
+        public bool IsEmpty() => this.root == null;
+
+
+        public void Insert(TKey key, TValue value)
+        {                                                                             //               10 Мария
+            TKey CurrentNode = this.root.Key;                                         //              /  \
+                                                                                      //           L /      R
+            if (CurrentNode.CompareTo(key) > 0) // this.Key > key => 1                //            /
+            {                                                                         //           4 Александр
+                if (this.root.Left == null)                                           //          / \
+                    this.root.Left = new BinarySearchTree<TKey, TValue>(key, value);  //         /   \ R
+                else                                                                  // Андрей 1     \
+                    this.root.Left.Insert(key, value);                                //         \     5 Катерина
+            }                                                                         //          \     \
+            else if (CurrentNode.CompareTo(key) < 0) // this.Key < key => -1          //   Татьяна 3     \
+            {                                                                         //          /       7 Наталья
+                if (this.root.Right == null)                                          //         /       / \
+                    this.root.Right = new BinarySearchTree<TKey, TValue>(key, value); //   Илья 2       /   \
+                else                                                                  //               /     9 Полина
+                    this.root.Right.Insert(key, value);                               //      Николай 6     /
+            }                                                                         //                   /
+            else this.root.Value = value;                                             //          Дмитрий 8
+        }
+
+
+        public String Traverse()
+        {
+            string result = String.Empty;
+
+            if (this.root != null)
+            {
+                if (this.root.Left != null) result = this.root.Left.Traverse();
+
+                result += $"{this.root.Value.ToString()}\n";
+
+                if (this.root.Right != null) result += this.root.Right.Traverse();
+            }
+            else throw new Exception("Binary tree doesn't contain elements!");
+            return result;
+        }
+
+
+        public TValue Find(TKey key)
+        {
+            TValue result = default;
+
+            if (this.root != null)
+            {
+                if (this.root.Key != null)
+                    if (this.root.Key.Equals(key)) return this.root.Value;
+                    else
+                    {
+                        if (this.root.Left != null)
+                            if (this.root.Key.CompareTo(key) > 0) return this.root.Left.Find(key);
+
+                        if (this.root.Right != null)
+                            if (this.root.Key.CompareTo(key) < 0) return this.root.Right.Find(key);
+                    }
+            }
+            else throw new Exception("Binary tree doesn't contain elements!");
+            return result;
+        }
+    }
+
+
     class Program
     {
         static void Main()
@@ -20,7 +109,8 @@ namespace BST
                 bst.Insert(p.Key, p.Value);
 
 
-            Console.WriteLine(bst.Traverse());
+            var result = bst.Traverse();
+            Console.WriteLine(result);
             /* Output results sorted by key:
             Андрей
             Илья
@@ -34,86 +124,15 @@ namespace BST
             Мария
             */
 
-            Console.WriteLine(bst.Find(5));
+            result = bst.Find(5);
+            Console.WriteLine(result);
             /* Output results found by key:
             Катерина
             */
 
-        }
-    }
-
-    class BinarySearchTree<TKey, TValue> where TKey : IComparable<TKey>
-    {
-        public TKey   Key   { get; private set; }
-        public TValue Value { get; private set; }
-
-        public BinarySearchTree<TKey, TValue> Left  { get; private set; }
-        public BinarySearchTree<TKey, TValue> Right { get; private set; }
-
-
-        public BinarySearchTree(TKey key, TValue value)
-        {
-            this.Key   = key;
-            this.Value = value;
-
-            this.Left  = null;
-            this.Right = null;
-        }
-
-
-        public void Insert(TKey key, TValue value)
-        {                                                                        //               10 Мария
-            TKey CurrentNode = this.Key;                                         //              /  \
-                                                                                 //           L /      R
-            if (CurrentNode.CompareTo(key) > 0) // this.Key > key => 1           //            /
-            {                                                                    //           4 Александр
-                if (this.Left == null)                                           //          / \
-                    this.Left = new BinarySearchTree<TKey, TValue>(key, value);  //         /   \ R
-                else                                                             // Андрей 1     \
-                    this.Left.Insert(key, value);                                //         \     5 Катерина
-            }                                                                    //          \     \
-            else if (CurrentNode.CompareTo(key) < 0) // this.Key < key => -1     //   Татьяна 3     \
-            {                                                                    //          /       7 Наталья
-                if (this.Right == null)                                          //         /       / \
-                    this.Right = new BinarySearchTree<TKey, TValue>(key, value); //   Илья 2       /   \
-                else                                                             //               /     9 Полина
-                    this.Right.Insert(key, value);                               //      Николай 6     /
-            }                                                                    //                   /
-            else this.Value = value;                                             //          Дмитрий 8
-        }
-
-
-        public String Traverse()
-        {
-            string result = String.Empty;
-
-            if (this.Left != null) result = this.Left.Traverse();
-
-            result += $"{this.Value.ToString()}\n";
-
-            if (this.Right != null) result += this.Right.Traverse();
-
-            return result;
-        }
-
-
-        public TValue Find(TKey key)
-        {
-            TValue result = default;
-
-            if (this.Key != null)
-            {
-                if (this.Key.Equals(key)) return this.Value;
-                else
-                {
-                    if (this.Left != null)
-                        if (this.Key.CompareTo(key) > 0) return this.Left.Find(key);
-
-                    if (this.Right != null)
-                        if (this.Key.CompareTo(key) < 0) return this.Right.Find(key);
-                }
-            }
-            return result;
+            //bst.Remove(10);
+            //result = bst.Traverse();
+            //Console.WriteLine(result);
         }
     }
 }
