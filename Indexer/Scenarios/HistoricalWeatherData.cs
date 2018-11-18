324b0152fbe148e6364792b18d51917c558970ad
Alexander Usov <28162428+sharpist@users.noreply.github.com>
2018-11-18 20:18:05 +0300

    C_Sharp

--- Indexer/Scenarios/HistoricalWeatherData.cs
+++ Indexer/Scenarios/HistoricalWeatherData.cs
@@ -1,5 +1,6 @@
 ﻿using IndexersSamples.Common;
 using System;
+// псевдоним сконструированного универсального типа с описательным именем
 using CityDataSample =
     System.Collections.Generic.Dictionary<System.String, System.Collections.Generic.Dictionary<System.DateTime, IndexersSamples.Common.Sample>>;
 using DateSample =
@@ -9,33 +10,41 @@ namespace IndexersSamples.SampleThree
 {
     struct HistoricalWeatherData
     {
+        /// <summary>
+        /// Моделирование типа двухмерного словаря, образующего внутреннее хранилище,
+        /// для управления историческими данными о температуре по двум аргументам:
+        /// городу (string) и дате (DateTime).
+        /// </summary>
         public Sample this[string city, DateTime date]
         {
             get
             {
                 var cityData = default(DateSample);
-
+                // выбрать в хранилище пару ключ-значение,
+                // инициализировать словарь cityData
                 if (!storage.TryGetValue(city, out cityData))
+                    // иначе выбросить исключение
                     throw new ArgumentOutOfRangeException(nameof(city), "City not found");
-
-                // извлечь любую часть времени
+                // получить дату в качестве ключа
                 var index = date.Date;
                 var sample = default(Sample);
-                if (cityData.TryGetValue(index, out sample))
-                    return sample;
-                throw new ArgumentOutOfRangeException(nameof(date), "Date not found");
+                return (cityData.TryGetValue(index, out sample)) ?
+                    sample :
+                    throw new ArgumentOutOfRangeException(nameof(date), "Date not found");
             }
             set
             {
                 var cityData = default(DateSample);
-
+                // выбрать в хранилище пару ключ-значение,
+                // инициализировать словарь cityData
                 if (!storage.TryGetValue(city, out cityData))
                 {
+                    // иначе создать словарь cityData,
                     cityData = new DateSample();
+                    // добавить в хранилище по ключу
                     storage.Add(city, cityData);
                 }
-
-                // извлечь любую часть времени
+                // получить дату в качестве ключа
                 var index = date.Date;
                 cityData[index] = value;
             }
--- Indexer/Scenarios/Program.cs
+++ Indexer/Scenarios/Program.cs
@@ -26,31 +26,31 @@ namespace IndexersSamples
             data["Chicago", new DateTime(1970, 6, 6)] = new Sample
             { Temp = 75, Pressure = 30.2 };
 
-            var item = data["Chicago", new DateTime(1970, 6, 6)];
-            Console.WriteLine(item.Temp);
+            var sample = data["Chicago", new DateTime(1970, 6, 6)];
+            Console.WriteLine(sample.Temp); // 75
 
-            item = data["Chicago", new DateTime(1970, 6, 6, 12, 30, 2)];
-            Console.WriteLine(item.Temp);
+            sample = data["Chicago", new DateTime(1970, 6, 6, 12, 30, 2)];
+            Console.WriteLine(sample.Temp); // 75
 
 
             data["Chicago", new DateTime(1970, 6, 6)] = new Sample
             { Temp = 85, Pressure = 30.2 };
 
-            item = data["Chicago", new DateTime(1970, 6, 6)];
-            Console.WriteLine(item.Temp);
+            sample = data["Chicago", new DateTime(1970, 6, 6)];
+            Console.WriteLine(sample.Temp); // 85
 
-            item = data["Chicago", new DateTime(1970, 6, 6, 12, 30, 2)];
-            Console.WriteLine(item.Temp);
+            sample = data["Chicago", new DateTime(1970, 6, 6, 12, 30, 2)];
+            Console.WriteLine(sample.Temp); // 85
 
 
             try
-            { item = data["New York", new DateTime(1980, 5, 12)]; }
+            { sample = data["New York", new DateTime(1980, 5, 12)]; }
 
             catch (ArgumentOutOfRangeException e)
             { Console.WriteLine(e.Message); } // перехвачено исключение
 
             try
-            { item = data["Chicago", new DateTime(1980, 5, 12)]; }
+            { sample = data["Chicago", new DateTime(1980, 5, 12)]; }
 
             catch (ArgumentOutOfRangeException e)
             { Console.WriteLine(e.Message); } // перехвачено исключение
