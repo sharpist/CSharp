d77cfb481ce64288af64a07ad8ef75b687755a89
Alexander Usov <webapp@programmer.net>
2020-10-13 04:20:33 +0300

    C_Sharp

--- Releasing Resources (objects)/IDisposable.cs
+++ Releasing Resources (objects)/IDisposable.cs
@@ -1,54 +1,61 @@
 ﻿// ШАБЛОН ДЛЯ БАЗОВОГО КЛАССА
 
-    public class Example : IDisposable
+public class UnmanagedClass : System.IDisposable
+{
+    // неуправляемый буфер памяти
+    private System.IntPtr buffer;
+    // управляемый ресурс
+    private System.Runtime.InteropServices.SafeHandle resource;
+
+    public UnmanagedClass()
     {
-        private IntPtr buffer;         // неуправляемый буфер памяти
-        private SafeHandle resource;   // управляемый ресурс
-        public Example()               // конструктор
-        {
-            this.buffer = ...
-            this.resource = ...
-        }
+        this.buffer = System.Runtime.InteropServices.Marshal.AllocHGlobal(1024);
+        //this.resource = ...
+    }
 
 
-        private bool disposed = false; // флаг
+    private bool disposed = false;
 
-        ~Example()                     // деструктор
-        { this.Dispose(false); }       // высвобождает неуправляемый ресурс (запуск деструктора, если объект не уничтожен до завершения программы)
+    ~UnmanagedClass()             // если объект не уничтожен до завершения программы
+    { this.Dispose(false); }      // высвобождает неуправляемый ресурс
 
-        public virtual void Dispose()  // высвобождает все ресурсы
-        {
-            this.Dispose(true);
-            GC.SuppressFinalize(this); // не вызывать метод завершения (предотвратить запуск деструктора)
-        }
-        protected virtual void Dispose(bool disposing)
-        {
-            lock (this)
+    public virtual void Dispose() // высвобождает все ресурсы
+    {
+        this.Dispose(true);
+        System.GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        lock (this) {
+            if (this.disposed) return;
+            this.disposed = true;
+
+            /* высвобождение неуправляемого ресурса */
+
+            System.Runtime.InteropServices.Marshal.FreeHGlobal(buffer);
+            buffer = System.IntPtr.Zero;
+
+            /* высвобождение управляемого ресурса */
+            if (disposing)
             {
-                if (!this.disposed)
-                {
-                    this.disposed = true;
-                    ReleaseBuffer(buffer); // высвобождение неуправляемого ресурса
-                    if (disposing)
-                    {
-                        if (resource != null) resource.Dispose(); // высвобождение управляемого ресурса
-                    }
-                }
+                if (resource is not null) resource.Dispose();
             }
         }
     }
+}
 
 /*
-Так как IDisposable полагается на явный вызов метода Dispose программистом (поскольку в рантайме он сам вызываться не будет),
-можно добавить финализатор/деструктор в свой класс для обеспечения автоматической, гарантированной очистки ресурсов:
+IDisposable полагается на явный вызов метода Dispose программистом в блоке finally (в рантайме он сам вызываться не будет),
+поэтому можно добавить деструктор (финализатор) в свой класс для обеспечения автоматической, гарантированной очистки ресурсов:
 в таком случае даже, если забыть о методе Dispose, GC всё сделает сам.
 
 ПРОИЗВОДНЫЙ КЛАСС, от класса, реализующего интерфейс IDisposable, не должен реализовывать интерфейс IDisposable,
 поскольку реализация метода IDisposable.Dispose базового класса наследуется производными классами.
 
 Необходимо:
-    Перегрузить override метод protected `` void Dispose(Boolean) чтобы он также вызывал метод Dispose(Boolean) базового класса
-    и передавал в него значение true. Для этого включить в метод последней инструкцию base.Dispose(disposing).
+	Перегрузить override метод protected `` void Dispose(Boolean) чтобы он также вызывал метод Dispose(Boolean) базового класса
+	и передавал в него значение true. Для этого включить в метод последней инструкцию base.Dispose(disposing).
 
-    Рекомендуется переопределить деструктор для производного класса + он должен вызывать перегруженный метод Dispose(false).
+	Рекомендуется переопределить деструктор для производного класса + он должен вызывать перегруженный метод Dispose(false).
 */
